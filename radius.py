defcea2f048c81e9cace2dcb07f7f4ee42d22840
hrithikkrish <80141827+hrithikkrish@users.noreply.github.com>
2021-03-06 10:25:01 +0530

    Update radius.py

--- radius.py
+++ radius.py
@@ -1,3 +1,3 @@
-r = float(input(" Enter the radius of the circle: ")
-area = pi*r*r
-print("area is",area)
+from math import pi
+r=float(input("radius os circle is:"))
+print("area is" + str(pi*r*r))
